98d399f5277897c1f4baa90321609adcc7990f5b
nhutdoan-code <ndn241126@gmail.com>
2026-03-17 23:20:32 +0700

    Fetch API & Axios_bai1+bai2+bai3

--- FetchAPI_bai03/src/App.jsx
+++ FetchAPI_bai03/src/App.jsx
@@ -1,31 +1,60 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { id } from 'react'
 
 function App() {
   const [data, setData] = useState([])
+  const [user, setUser] = useState(null)
+  const [id, setId] = useState("")
+  const [error, setError] = useState("")
+  var url = `https://jsonplaceholder.typicode.com/users/${id}`;
 
   useEffect(() => {
+    if (id==="") {
+      return;
+    }
+
+    //Ép kiểu sang number để so sánh
+    const numId = Number(id);
+
+    if (numId<1 || numId>10) {
+      setError("User not found")
+      setUser(null);
+      return;
+    }
+    setError("")
+
     var fetchdata = fetch(url);
     var res = fetchdata.then((response) => {
       return response.json();
-    }).then((data) => {
-      setData(data);
-      console.log(data);
-
     })
-  }, [])
+    .then((data) => {
+      if (!data.id) {
+          setError("User not found")
+          setUser(null)
+        } else {
+          setUser(data)
+        }
+    })
+    .catch(()=>{
+      setError("User not found")
+      setUser(null)
+    })
+  }, [id])
   return (
     <>
+      <input type="number" placeholder='Nhập userId(1-10)' value={id} onChange={(e) => setId(e.target.value)}/>
+      {error && <p style={{ color: "red" }}>{error}</p>}
       {
-      data.map((item)=>{
-        return <div key={item.id}>
-          <p>{item.name}</p>
-          <p>{item.phone}</p>
-          <p>{item.website}</p>
+      user && (
+        <div>
+          <p>{user.name}</p>
+          <p>{user.phone}</p>
+          <p>{user.website}</p>
         </div>
-      })
+      )
     }
     </>
   )
